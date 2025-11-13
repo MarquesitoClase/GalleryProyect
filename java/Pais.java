271f250de9008295c173e6a8bd0719a8f0833d80
MarquesitoClase <juan.luis.marquezcan@gmail.com>
2025-11-13 18:37:43 +0100

    First commit imaginary land

---
+++ java/Pais.java
@@ -0,0 +1,14 @@
+package java;
+
+public class Pais {
+    private String nombre = "Numerlandia";
+    private int nHabitantes;
+
+    public String getNombre(){
+    return nombre;
+}
+
+public int getNHabitantes(){
+    return nHabitantes;
+}
+}
